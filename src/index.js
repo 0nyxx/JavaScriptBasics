d3d2e28bc8e2f6310c5bc5906d44374631c96ec8
0nyxx <81373079+0nyxx@users.noreply.github.com>
2022-03-07 21:42:07 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const nev = "Arti";
+let kor = 15;
+let magassag = 170;
+console.log(nev, kor, magassag);
+//.
+
+let szam1 = 32;
+let szam3 = 5;
+console.log(szam1 + szam3);
+//..
+
+const str = "Helló,";
+console.log(str + " " + nev + "!");
+//datum
+const date = new Date();
+console.log(date);
